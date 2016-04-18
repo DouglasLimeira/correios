d98b5d2a1c6cf1175fb1059b9f2a857639c33715
Douglas Limeira Silva <douglas.limeira.silva@gmail.com>
2016-04-18 19:17:26 -0300

    Retirada de métodos não utilizados

--- correios/src/br/com/correios/bo/CorreiosBO.java
+++ correios/src/br/com/correios/bo/CorreiosBO.java
@@ -5,7 +5,6 @@ import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.HtmlEmail;
 
 import br.com.correios.model.ICorreios;
-import br.com.correios.util.FotoUtil;
 
 public class CorreiosBO implements ICorreios{
 	private static final String HOST_NAME = "mail.imobiliariapp.com.br";
@@ -26,35 +25,5 @@ public class CorreiosBO implements ICorreios{
 		htmlEmail.setHtmlMsg(mensagemCorpo);
 		htmlEmail.addTo(emailUsuario);
 		htmlEmail.send();
-	}
-	
-	public void enviarEmailBoasVindas(String emailParaEnvio) {
-		try {
-			HtmlEmail email = new HtmlEmail();
-			email.setHostName(HOST_NAME);
-			email.setSmtpPort(SMTP_PORT);
-			email.setAuthenticator(new DefaultAuthenticator(MAIL_CONTATO, USER_PASS));
-			email.setSSLOnConnect(false);
-			email.setFrom(MAIL_CONTATO, "Contato iMobiliáriapp");
-			email.setSubject("Bem vindo ao iMobiliáriapp");
-
-			String caminhoPastaComum = FotoUtil.getCaminhoPastaComum();
-			String caminhoPastaBemVindoMail = FotoUtil.getCaminhoPastaBemVindoMail();
-			
-			StringBuilder htmlMsg = new StringBuilder();
-			htmlMsg.append("<html><body><div style=\"width: 800px; margin:auto;\">");
-			htmlMsg.append("<img src=\""+ caminhoPastaComum + "topo.jpg\" style=\"width:800px;\"/> </br>");
-			htmlMsg.append("<img src=\""+ caminhoPastaComum + "ola.jpg\" style=\"width:800px;\"/> </br>");
-			htmlMsg.append("<img src=\""+ caminhoPastaBemVindoMail + "bem_vindo_msg.jpg\" style=\"width:800px;\"/> </br>");
-			htmlMsg.append("<img src=\""+ caminhoPastaComum + "footer.jpg\" style=\"width:800px;\"/>");
-			htmlMsg.append("</div></body></html>");
-
-			email.setHtmlMsg(htmlMsg.toString());
-			email.setTextMsg("Olá, recebemos suas informações e estamos fazendo a análise. Aguarde, em breve receberá  o e-mail de resposta");
-			email.addTo(emailParaEnvio);
-			email.send();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 	}
 }
